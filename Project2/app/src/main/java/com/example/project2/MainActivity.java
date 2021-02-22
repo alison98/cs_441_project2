3d5bc8627540be5860a4d1e9ccdae1761592672e
alison98 <nosila98@gmail.com>
2021-02-22 10:28:55 -0500

    add second activity page

---
+++ Project2/app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        next = findViewById(R.id.next_button);
+    }
+
+
+    public void switch_action(View view){
+        Intent activity2 = new Intent(this, Activity2.class);
+        startActivity(activity2);
+    }
+}
