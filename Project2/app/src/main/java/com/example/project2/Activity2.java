52c11fb63229142b153352616bfafa634f846d52
alison98 <nosila98@gmail.com>
2021-02-24 11:48:32 -0500

    redo second screen

--- Project2/app/src/main/java/com/example/project2/Activity2.java
+++ Project2/app/src/main/java/com/example/project2/Activity2.java
@@ -1,33 +1,21 @@
 package com.example.project2;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.settings, new SettingsFragment())
-                    .commit();
-        }
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        setContentView(R.layout.activity_2);
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
+    public void switch_main(View view){
+        Intent main_activity = new Intent(this, MainActivity.class);
+        startActivity(main_activity);
     }
 }
